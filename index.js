d7030b7df658ee510a0b4107f13f6f8db345070c
Christian's Mac <macbook@Macbooks-MacBook-Pro.local>
2019-05-20 15:52:20 -0600

    add changes.  get students

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
+require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
+const passport = require('passport');
+const strategy = require(`${__dirname}/strategy.js`);
+const students = require('./students.json');
 
 const app = express();
+
+app.use( session({
+    secret: process.env.CLIENT_SECRET,
+    resave: false,
+    saveUninitialized: false
+  }));
+  app.use( passport.initialize() );
+  app.use( passport.session() );
+  passport.use( strategy );
+
+  passport.serializeUser(function(user, done) {
+    done(null, { id: user.id, display: user.displayName, nickname: user.nickname, email: user.emails[0].value });
+  });
+  
+  passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+  });
+
+  app.get( '/login',
+  passport.authenticate('auth0',
+    { successRedirect: '/students', failureRedirect: '/login', connection: 'github' }
+  )
+);
+
+function authenticated(req, res, next){
+
+}
+
+app.get('/students', ( req, res, next) => {
+      res.status(200).send( students );
+});
 
 const port = 3000;
 app.listen( port, () => { console.log(`Server listening on port ${port}`); } );
